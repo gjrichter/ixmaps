08c62264f3c72d11c5b3a0c347c6f2565c1a58ee
gjrichter <guenter.richter@gmail.com>
2018-11-20 19:32:35 +0100

    new data processing for example

--- app/Palermo_Incidenti/stories/2016/process.js
+++ app/Palermo_Incidenti/stories/2016/process.js
@@ -7,6 +7,10 @@
 	ixmaps.totale_incidenti_georef = ixmaps.totale_incidenti_georef || {};
 	ixmaps.totale_incidenti_georef.process = function(dbTable){
 
+		dbTable.column("ORA").map(value => {
+			return parseInt(value);
+		});
+
 		dbTable.addColumn({'source':'DATA','destination':'GiornoDellaSettimana'},function(value){
 			var dateA = value.split('/');
 			var date = new Date(Number(dateA[2]),Number(dateA[1])-1,Number(dateA[0]));
